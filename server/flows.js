2c47ef7772927d2411117a06509526d7dd3f872f
Kotcharat <mosmoth04@gmail.com>
2024-01-09 19:18:01 +0700

    🚧Update : Add Flow and check name and path in /add

--- server/flows.js
+++ server/flows.js
@@ -1,37 +1,46 @@
 const express = require('express');
-const router = express.Router();
 const flowModel = require('./models/flowModel');
+const router = express.Router();
 
+// Get Flow from workspace_id
+try{
 router.get('/get', async (req, res) => {
-  const { workspaceid } = req.query;
-
-  const result = await flowModel.findAll({
-    attributes: [
-      "id",
-      "name",
-      "description",
-      "API",
-      "markdown",
-      "status"
-    ],
-    where: {
-      workspace_id: workspaceid
-    }
-  });
-
-  for (let i = 0; i < result.length; i++) {
-    const flow = result[i];
-
-    if (flow.dataValues.status === 1) {
-      flow.dataValues.isOnline = true;
-    } else {
-      flow.dataValues.isOnline = false;
-    }
-    
-    delete flow.dataValues.status;
+  const {workspaceid} = req.query
+ const result = await flowModel.findAll({
+    attributes:["id","name","description","API","markdown","status"],
+    where : {workspace_id : workspaceid }
+  })
+  return res.status(200).send({ status: true , data : result });
+});
+}catch(err){
+  console.error("Error get Flow:", err);
+    return res.status(500).send({ msg: "Error get Flow", status: false });
+}
+// Add Table Flow 
+try {
+router.post("/add", async (req,res) => {
+  const {name ,description,API,markdown,status,workspace_id } = req.body;
+  const check = await flowModel.findOne({where:{name,API}});
+  if(check){
+    return res.status(200).send({status: false ,msg: "Flow name or path is already used !"})
+  }
+  const newFlow = await flowModel.create({
+    name:name,
+    description:description,
+    API:API,
+    markdown:markdown,
+    status:status,
+    workspace_id:workspace_id
+  })
+  const FlowID = newFlow.id;
+  console.log('Create Flow with ID : ', FlowID)
+  return res.status(200).send({status : true,flow_id:FlowID })
+})
+} catch(err){
+  console.error("Error creating Flow:", err);
+    return res.status(500).send({ msg: "Error creating Flow", status: false });
   }
 
-  return res.status(200).send({ status: true, data: result });
-});
 
 module.exports = router;
+
