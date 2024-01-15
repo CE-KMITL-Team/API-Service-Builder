c217adcfad29c5c340051a0738eb5911c5bf98d6
Chidsanu_ <Chidsanupongboonma@gmail.com>
2024-01-15 18:48:35 +0700

    🚧Update: Flows

--- server/routers/flows.js
+++ server/routers/flows.js
@@ -4,24 +4,24 @@ const flowModel = require("../models/flowModel");
 // const workspaceModel = require("../../models/workspaceModel");
 
 // Get Flow from workspace_id
-try {
-  router.get("/get", async (req, res) => {
-    const { workspaceid } = req.query;
+router.get("/get", async (req, res) => {
+  const { workspaceid } = req.query;
+  try {
     const result = await flowModel.findAll({
       attributes: ["id", "name", "description", "API", "markdown", "status"],
       where: { workspace_id: workspaceid },
     });
     return res.status(200).send({ status: true, data: result });
-  });
-} catch (err) {
-  console.error("Error get Flow:", err);
-  return res.status(500).send({ msg: "Error get Flow", status: false });
-}
+  } catch (err) {
+    console.error("Error get Flow:", err);
+    return res.status(500).send({ msg: "Error get Flow", status: false });
+  }
+});
 
 // Add Table Flow
-try {
-  router.post("/add", async (req, res) => {
-    const { name, description, API, markdown, status, workspace_id } = req.body;
+router.post("/add", async (req, res) => {
+  const { name, description, API, markdown, status, workspace_id } = req.body;
+  try {
     const check = await flowModel.findOne({ where: { name, API } });
     if (check) {
       return res
@@ -39,16 +39,16 @@ try {
     const FlowID = newFlow.id;
     console.log("Create Flow with ID : ", FlowID);
     return res.status(200).send({ status: true, flow_id: FlowID });
-  });
-} catch (err) {
-  console.error("Error creating Flow:", err);
-  return res.status(500).send({ msg: "Error creating Flow", status: false });
-}
+  } catch (err) {
+    console.error("Error creating Flow:", err);
+    return res.status(500).send({ msg: "Error creating Flow", status: false });
+  }
+});
 
 // Check Flows name and path is exists
-try {
-  router.post("/check-detail", async (req, res) => {
-    const { flow_name, flow_path } = req.body;
+router.post("/check-detail", async (req, res) => {
+  const { flow_name, flow_path } = req.body;
+  try {
     const checkname = await flowModel.findOne({ where: { name: flow_name } });
     const checkpath = await flowModel.findOne({ where: { API: flow_path } });
     if (checkname && checkpath) {
@@ -65,33 +65,38 @@ try {
         .send({ status: false, errorName: false, errorPath: true });
     }
     return res.status(200).send({ status: true });
-  });
-} catch (error) {
-  console.error("Error:", error);
-  return res.status(500).send("Internal Server Error");
-}
+  } catch (error) {
+    console.error("Error:", error);
+    return res.status(500).send("Internal Server Error");
+  }
+});
 
 // Delete Flow from workspace_id and flow_id
 router.delete("/delete", async (req, res) => {
   const { workspace_id, flow_id } = req.body;
-  const flow_name = await flowModel.findOne({
-    attributes: ["name"],
-    where: {
-      id: flow_id,
-    },
-  });
-  const deleteFlow = await flowModel.destroy({
-    where: {
-      workspace_id: workspace_id,
-      id: flow_id,
-    },
-  });
+  try {
+    const flow_name = await flowModel.findOne({
+      attributes: ["name"],
+      where: {
+        id: flow_id,
+      },
+    });
+    const deleteFlow = await flowModel.destroy({
+      where: {
+        workspace_id: workspace_id,
+        id: flow_id,
+      },
+    });
 
-  if (deleteFlow === 0) {
-    return res.status(200).send({ status: false, msg: "Flow not found" });
+    if (deleteFlow === 0) {
+      return res.status(200).send({ status: false, msg: "Flow not found" });
+    }
+    
+    return res.status(200).send({ status: true, flow_name: flow_name.name });
+  } catch (err) {
+    console.error("Internal server error:", err);
+    return res.status(500).send("Internal Server Error");
   }
-
-  return res.status(200).send({ status: true, flow_name: flow_name.name });
 });
 
 /* Edit Flows */
@@ -115,12 +120,10 @@ router.put("/edit", async (req, res) => {
     }
 
     if (check.name == name || check.API == API) {
-      return res
-        .status(200)
-        .send({
-          status: false,
-          msg: "Flow name or API-Path is already used !",
-        });
+      return res.status(200).send({
+        status: false,
+        msg: "Flow name or API-Path is already used !",
+      });
     }
 
     await flowModel
